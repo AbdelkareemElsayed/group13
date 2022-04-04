ca829d1a84463e4a3342bc354c8540c5bd681355
MrWeber94 <abdelkareemelsayed2014@gmail.com>
2022-04-04 15:37:18 +0200

    v4.4

---
+++ session1.php
@@ -0,0 +1,163 @@
+<?php 
+
+ //  print "welcome to php course"; 
+
+/*
+ print "welcome to php course"; 
+ print "welcome to php course"; 
+ print "welcome to php course"; 
+ print "welcome to php course"; 
+ print "welcome to php course"; 
+*/
+
+ # Used to print message .... 
+ // echo "welcome to php course";   
+
+/*
+
+   ==  Data Types ....  ==
+  int 
+  doubles 
+  boolean 
+  null 
+  string 
+  array 
+  object    oop 
+  resources 
+*/
+
+// $age = 20; 
+
+// $name = "Root Account"; 
+//   var_dump($name);
+
+// $age = 18 ; 
+// echo $age;    // 18 
+
+
+// define("size",50);
+// // echo size;
+// echo constant('size');
+
+
+/*
+ + 
+ -  
+ / 
+ % 
+ *
+ ** 
+*/
+
+
+// $num1 = 2; 
+// $num2 = 3 ; 
+
+// echo ($num1 + $num2);
+
+// $num1 = 4 ; 
+ 
+// // $num1 =  $num1 - 2;      // 2 
+
+//    $num1 -=2;    // $num1 - 2;
+
+// echo $num1; 
+
+
+
+
+// $num1 = 1; 
+// $num2 = "2" ; 
+
+
+// var_dump((bool)$num1); 
+
+
+//   var_dump($num1 === $num2);
+
+/*
+> 
+< 
+>= 
+<=
+==     (value)
+===    (value & datatype)
+!=     (value)
+!==    (value & datatype)
+*/
+
+
+
+
+// $num1 = 2; 
+// echo  $num1 = $num1+1;   // $num1 += 1; 
+// echo  $num1 = $num1-1;   // $num1 -= 1; 
+//  echo   $num1++;    // post inc .... 
+//  echo    ++$num1;    // pre inc  
+// echo --$num1; 
+// echo $num1;  
+
+
+
+// AND OR   !    XOR ... 
+
+
+/*
+
+  cond1      OP       cond2          Result  
+  true      && AND      true            true 
+  true      && and      false           false 
+  false     && and      true            false
+  false     && and      fasle           false 
+
+
+
+
+  cond1         OP       cond2          Result  
+  true         || or      true            true 
+  true         || or      false           true
+  false        || or      true            true
+  false        || or      fasle           false 
+
+
+
+
+  cond1         OP       cond2          Result  
+  true          xor      true            false 
+  true          xor      false           true
+  false         xor      true            true
+  false         xor      fasle           false
+
+
+! 
+*/
+
+
+
+
+//    var_dump( !false); 
+
+
+// $text = "welcome"; 
+// $name = "Root Account"; 
+
+
+// $message = $text.$name; 
+
+
+// echo $message;
+
+
+  
+
+
+
+?>
+
+
+
+
+
+
+
+
---
+++ session2.php
@@ -0,0 +1,277 @@
+<?php 
+// code .... 
+
+//  echo 'php course';
+
+
+
+//  echo 'js course';
+
+
+//  $age = 7; 
+
+ 
+//  if($age >= 20 ){
+//    //  code .....
+   
+//    echo 'Age >= 20';
+
+//  }elseif($age < 20  && $age >= 10 ){
+//      // code .... 
+//      echo 'Age between 10 & 20';
+//  }elseif($age < 10 && $age >= 5){
+//     echo 'Age between 5 & 10';
+//  } else{
+//    // code ..... 
+//    echo 'Age < 10';
+//  }
+
+//  echo 'js course';
+
+
+
+//  $color = "x"; 
+
+
+// switch ($color) {
+//     case 'blue':
+//         # code...
+//         echo 'Color Is Blue'; 
+//         break;
+
+//     case 'green': 
+//         # Code ... 
+//         echo "Color Is Green";
+//         break; 
+        
+//     case 'red': 
+//             # Code ... 
+//             echo "Color Is RED";
+//             break; 
+        
+            
+//     default:
+//         # code...
+//         echo 'No Matched Color';
+//         break;
+// }
+
+
+// $age = 4; 
+
+// switch ($age) {
+    
+//     case ($age >= 20):
+//         # code...
+//         echo 'Age >= 20';
+//         break;
+
+//     case ($age < 20  && $age >= 10 ): 
+//         # code ... 
+//         echo 'Age Between 20 & 10'; 
+//         break; 
+   
+//      case ($age < 10  && $age >= 5 ): 
+//             # code ... 
+//             echo 'Age Between 5 & 10'; 
+//             break; 
+
+//     default:
+//         # code...
+//         echo 'Age < 5';
+//         break;
+// }
+
+
+
+
+
+
+//    echo 'php course';
+//    echo 'php course';
+//    echo 'php course';
+//    echo 'php course';
+
+//    for ($i=0; $i < 5 ; $i++) { 
+//        # code...
+//        echo 'php course <br>';
+
+//    }
+
+// $i = 0; 
+
+// while ($i < 5) {
+//     # code...
+
+//     echo 'php course <br>';
+
+//     $i++; 
+// }
+
+// $i = 0; 
+// do {
+    
+//     # code...
+//     echo 'php course <br>';
+//     $i++;
+// } while ($i < 5 );
+
+
+
+
+  // break  vs  continue 
+
+
+
+//    for ($i=0; $i < 5 ; $i++) { 
+//        # code...
+
+//        if($i == 2 ){
+
+//         continue;
+
+//        }
+
+//        echo $i.' - php course <br>';
+
+//    }
+
+
+
+// $message = "php  course";
+
+
+// echo '<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// </head>
+// <body>
+
+//    <h1>Html Conetnt</h1>'.
+ 
+//    $message
+
+
+// .'</body>
+// </html>';
+
+
+// ()   []     { }  
+
+//   function message($name,$phone = null)
+//   {
+//         // code ..... 
+//          'Name = '.$name.' & phone = '.$phone.'<br>';
+//   }
+
+
+//    echo   message('Root'); 
+
+//     $age = 20; 
+
+//     echo   message('X','015xxx'); 
+
+
+// declare(strict_types=1);
+
+
+// function AddNumbers(int $num1,int $num2): int {
+      
+//      return $num1 + $num2; 
+// }
+
+// AddNumbers(12,3);
+
+
+
+// echo $num1;
+
+
+// global scope .... 
+// $age = 20; 
+
+// // echo $age; 
+
+// function message(){
+//   //  $message  = "php course";      // local scope .... 
+
+//    global $age;
+//     echo $age; 
+
+// }
+// message();
+
+
+// $text = "php course"; 
+
+
+//  $output = ltrim($text);
+// $output = rtrim($text);
+//    $output = trim($text);
+//   echo strlen($output);    strlen(trim($text));  
+
+//   echo  str_word_count($text);
+
+//  echo  strrev($text); 
+
+//   echo  strpos($text,'course'); 
+// echo str_replace("php","js",$text);
+
+// $output  =   explode(' ',$text);
+// var_dump($output); 
+
+// $students = ["x","y","z"];
+
+//   echo     implode("-",$students);
+
+
+// $age = 10.1; 
+
+// var_dump(is_int($age));
+// var_dump(is_float($age));
+// var_dump(is_numeric($age));
+
+
+// echo pi();
+
+    
+
+//    echo  max(2,-1,10,55,200,8);
+// echo  min(2,-1,10,55,200,8);
+
+//   echo  sqrt(64);
+
+//   echo   round(0.7);
+
+//   echo  rand(1,20);
+
+
+
+
+//  function test(){
+
+//     static  $x = 0; 
+     
+//      echo $x.'<br>'; 
+
+//      $x++;
+
+//  }
+
+//  test();   //     0
+//  test();   //     1 
+//  test();  //      2 
+
+
+
+
+
+
+
+?>
+
+
