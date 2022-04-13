606f2858bfb52a621b8943eff08d3cd8cb1a598b
MrWeber94 <abdelkareemelsayed2014@gmail.com>
2022-04-13 18:54:54 +0200

    abdelkareem

--- week2/session4.php
+++ week2/session4.php
@@ -6,7 +6,8 @@
 1:m  == (m:1)
 m:m 
 
-=============================================================== 
+
+==============================================================
 
 students [name,email,password,phone]
 Managers  [name,email]
@@ -73,11 +74,6 @@ managers        departmnets
  1     :          1 
 
 
-
-
-
-
-
 # Task .... 
 Hospital management system that have 3 main types of users 1-admins 2-doctors 3-Patients.
 With the following data.
@@ -92,7 +88,108 @@ Note : doctor can accept or refuse reservations.
 Requirments : create a database structure.
 
 
+
+# admin 
+id name    email      password
+1   admin   admin@a    123 
+
+# patient 
+id name  email  password
+1   p     p@p    145
+
+# doctors 
+id name  email  password    specialize     gender   
+1  doc    c@d     7777        x             male 
+
+#########################################################################################
+
+# UserTypes 
+id     title 
+1      admin 
+2      doctor 
+3      patient 
+
+# Users 
+id    name     email      password    type_id      
+1     x         x@x        123          2             
+2     y         y@y        888          3     
+3     a         a@a        777          1   
+4     d         d@d        999          2    
+
+
+# doctor details 
+id    specialize     gender        user_id(doc_id)
+1        sp_x         male           1
+2        sp_y         female         4 
+
+
+users     UType 
+1          1 
+m          1
+============= 
+m      :   1 
+
+
+
+users(doctor)        DocDetails
+1                       1
+1                       1
+========================== 
+1                       1
+ 
+
+
+#########################################################################################
+
+# Appointments 
+id   day      from        to   
+1    sun       12:00 pm   12:30pm       
+2    sun       12:30 pm   1:00pm
+
+
+
+
+
+doctor        appo 
+1              m 
+m              1
+===================== 
+m              m 
+
+
+
+# Doctor Appointments 
+id       doc_id       app_id        
+1         1             2     
+
+
+
+patient         doc_appo 
+1                  m 
+m                  1
+======================== 
+m      :            m 
+
+
+#  Patient Reservation ..... 
+id      pat_id    doc_app_id      date       status 
+1        1           1            13/4        flase 
  */
 
+
+
+
+
+
+
+
+// orders 
+// id   price   date     user_id 
+// 1    200.50   13/4     2 
+
+// orderDetails 
+// id   product_id   q 
+// 1     2           3
+// 2     4           2
 
 ?>
