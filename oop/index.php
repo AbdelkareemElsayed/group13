7ad09b1520d54b3d45789a36524413acaf20ba31
MrWeber94 <abdelkareemelsayed2014@gmail.com>
2022-04-20 14:59:30 +0200

    abdelkareem

--- oop/index.php
+++ oop/index.php
@@ -1,77 +1,77 @@
 <?php
 // oop .... 
 
-class user{
-    var $name; 
-    var $email; 
+// class user{
+//     var $name; 
+//     var $email; 
 
 
-    function  __construct($val1,$val2){
+//     function  __construct($val1,$val2){
         
-        $this->name  = $val1;
-        $this->email = $val2;
+//         $this->name  = $val1;
+//         $this->email = $val2;
 
-    }
+//     }
 
 
 
      
-    // function setName($val){
-    //      $this->name = $val;
-    // }
+//     // function setName($val){
+//     //      $this->name = $val;
+//     // }
 
 
-    function getName(){
-        return $this->name;
-    }
+//     function getName(){
+//         return $this->name;
+//     }
 
 
 
 
         
-//     function setEmail($val){
-//         $this->email = $val;
+// //     function setEmail($val){
+// //         $this->email = $val;
+// //    }
+
+
+//    function getEmail(){
+//        return $this->email;
 //    }
 
 
-   function getEmail(){
-       return $this->email;
-   }
+
+//     function Message($input){
+//         return 'Welcome to OOP , '.$input;
+//     }
+
+
+//     function __destruct()
+//     {
+//         echo 'Destruct Method';
+//     }
 
 
 
-    function Message($input){
-        return 'Welcome to OOP , '.$input;
-    }
-
-
-    function __destruct()
-    {
-        echo 'Destruct Method';
-    }
+// }
 
 
 
-}
+// $admin = new user("Root Account","Root@yahoo.com"); 
+
+// //   echo $admin->Message("ADMIN"); 
 
 
+//      # SET ADMIN NAME .... 
+//     //  $admin->setName("Root Account"); 
+//      # GET NAME ..... 
+//      ECHO  $admin->getName().'<BR>';
 
-$admin = new user("Root Account","Root@yahoo.com"); 
-
-//   echo $admin->Message("ADMIN"); 
-
-
-     # SET ADMIN NAME .... 
-    //  $admin->setName("Root Account"); 
-     # GET NAME ..... 
-     ECHO  $admin->getName().'<BR>';
-
-     # set & get email ... 
-    //  $admin->setEmail("root@yahoo.com"); 
-     ECHO $admin->getEmail();
+//      # set & get email ... 
+//     //  $admin->setEmail("root@yahoo.com"); 
+//      ECHO $admin->getEmail();
 
 
-  echo '<br>';
+//   echo '<br>';
 
 // $student = new user("Student Accoutn","Student@gmail.com"); 
 
@@ -84,6 +84,150 @@ $admin = new user("Root Account","Root@yahoo.com");
 
 // echo $student->Message("USER"); 
 
+
+
+/*
+ admin   [name,email,passowrd] - {Login , Register , AcceptStudent}
+ student [name,email,password,level] - {Login,Register,JoinCours} 
+
+*/
+
+// trait Messages {
+
+//     function sendSMS(){
+//         echo 'Send SMS TO USER ... ';
+//     }
+
+// }
+
+
+
+
+/*    
+                      same class         object             extends 
+  1- public             true              true               true 
+  2- private            true              false              false 
+  3- protected          true              false              true 
+  
+
+*/
+
+
+
+# Parent class || super class 
+ class user {
+
+    public $name = "account"; 
+    private $email = "test@yahoo" ; 
+    protected $password = "tech123"; 
+
+
+
+    public function setEmail($email){
+        $this->email = $email;
+    }
+
+
+    public function getEmail(){
+       return $this->email ;
+    }
+
+
+   final  function login()
+    {
+
+        // email && password 
+        echo 'Login Method .... ';
+    }
+
+
+
+   public function Register(){
+        echo 'Register Method ..... ';
+
+    }
+}
+
+
+############################################################################## 
+
+// $user = new user(); 
+// // $user->email = "testaccount@yahoo.com"; 
+// // echo $user->email;
+
+// // $user->setEmail("test@yahoo");
+// // echo $user->getEmail();
+
+// $user->password = "1213";
+// echo $user->password;
+// exit(); 
+
+
+
+# sub class , child class , derived class 
+ class admin extends  user {
+
+//     use Messages; 
+
+    // function login()
+    // {
+    //     // email && password 
+    //     echo 'Login Method   Admin .... '.$this->password;
+    // }
+
+
+    function AcceptStudent(){
+
+    }
+
+
+}
+
+
+class student extends user {
+
+
+    var $level; 
+
+
+    function JoinCours(){
+        
+    }
+
+
+}
+
+
+
+$admin = new admin; 
+
+// // $admin->name = "admin account"; 
+// // echo $admin->name;
+$admin->login();
+
+// // $admin->sendSMS();
+
+// echo '<br>';
+
+// $std = new student; 
+// $std -> login();
+
+
+
+
+
+
+
+//   class x{
+    
+//     private function __construct()
+//      {
+//             return true; 
+//      }
+//   }
+//   $obj = new x;
+
+//   var_dump($obj);
 
 
 
